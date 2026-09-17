dec91b22889d4932824a38c6aa488710443ebe39
ZinexDa <224714749+ZinexDa@users.noreply.github.com>
2026-09-17 14:58:05 +1000

    Bump version to 1.1.1 (v1.1.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.sonicr.android"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.1.1"
 
         externalNativeBuild {
             cmake {
